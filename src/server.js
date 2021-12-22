6b9513f99e046fc4af6d70b4fd27ee1d11564c30
Naveen Dongre <naveendongre4737@gmail.com>
2021-12-23 00:51:24 +0530

    tried to solve that app error Please enter the commit message for your changes. Lines starting

--- src/server.js
+++ src/server.js
@@ -20,11 +20,12 @@ io.on('connection', (socket) => {
 
 if(process.env.NODE_ENV === "production"){
   console.log("iosdn");
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, './frontend/build')));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "./frontend", "build", "index.html"))
   })
-}else{
+}
+else{
   app.get("/", (req, res) => {
     console.log(process.env.NODE_ENV)
     res.send("api running");
